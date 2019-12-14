f23691d7749232256aede6e6d1cb453c1190bebf
FaisalFehad <faisal.fehad@pm.me>
2019-12-14 13:22:53 +0000

    add formatComments util

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -18,4 +18,27 @@ exports.makeRefObj = list => {
   return newRef;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  const commentsArr = [];
+  if (!comments) return commentsArr;
+
+  comments.forEach(comment => {
+    let newComment = { ...comment };
+
+    // changes created_by to author id
+    newComment.author = newComment.created_by;
+    delete newComment.created_by;
+
+    // changes belongs_to to article_id
+    newComment.article_id = articleRef[newComment.belongs_to];
+    delete newComment.belongs_to;
+
+    // The value of the new article_id key must be the id corresponding to the original title value provided
+
+    // Its created_at value converted into a javascript date object
+    newComment.created_at = new Date(newComment.created_at);
+
+    commentsArr.push(newComment);
+  });
+  return commentsArr;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -116,7 +116,7 @@ describe("formatDates", () => {
   });
 });
 
-describe.only("makeRefObj", () => {
+describe("makeRefObj", () => {
   it("returns an empty object when passed an empty array", () => {
     expect(makeRefObj([])).to.deep.equal({});
   });
@@ -136,4 +136,248 @@ describe.only("makeRefObj", () => {
   });
 });
 
-describe("formatComments", () => {});
+describe.only("formatComments", () => {
+  it("returns a new array when invoked with no arguments", () => {
+    expect(formatComments()).to.deep.equal([]);
+  });
+
+  describe("takes a comments and reference object, and returns newly formated comments", () => {
+    const testComments = [
+      {
+        title: "Stone Soup",
+        topic: "cooking",
+        author: "cooljmessy",
+        body: "The first day I pu...",
+        created_at: 1481662720516
+      },
+      {
+        title: "The vegan carnivore?",
+        topic: "cooking",
+        author: "tickle122",
+        body: "The chef Richst...",
+        created_at: 1492163783248
+      }
+    ];
+    const testRef = makeRefObj(testComments);
+
+    it("returns a renamed created_by property into author", () => {
+      const testArticles = [
+        {
+          article_id: 1,
+          title: "Agility Training....",
+          topic: "cooking",
+          author: "cooljmessy",
+          body: "The first day I pu...",
+          created_at: 1481662720516
+        },
+        {
+          article_id: 2,
+          title: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          topic: "cooking",
+          author: "tickle122",
+          body: "The chef Richst...",
+          created_at: 1492163783248
+        }
+      ];
+
+      const testComments = [
+        {
+          body: "Praesentium pariatur a nisi aut.",
+          belongs_to: "Agility Training....",
+          created_by: "grumpy19",
+          votes: 11,
+          created_at: 1462414995595
+        },
+        {
+          body: "Voluptatum aut fac",
+          belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          created_by: "weegembump",
+          votes: -4,
+          created_at: 1477955052607
+        }
+      ];
+
+      const testRef = makeRefObj(testArticles);
+      const actual = formatComments(testComments, testRef);
+      expect(actual[0]).to.not.have.keys("created_by");
+      expect(actual[0]).to.contain.keys("author");
+    });
+
+    it("returns a renamed belongs_to property to article_id", () => {
+      const testArticles = [
+        {
+          article_id: 1,
+          title: "Agility Training....",
+          topic: "cooking",
+          author: "cooljmessy",
+          body: "The first day I pu...",
+          created_at: 1481662720516
+        },
+        {
+          article_id: 2,
+          title: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          topic: "cooking",
+          author: "tickle122",
+          body: "The chef Richst...",
+          created_at: 1492163783248
+        }
+      ];
+
+      const testComments = [
+        {
+          body: "Praesentium pariatur a nisi aut.",
+          belongs_to: "Agility Training....",
+          created_by: "grumpy19",
+          votes: 11,
+          created_at: 1462414995595
+        },
+        {
+          body: "Voluptatum aut fac",
+          belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          created_by: "weegembump",
+          votes: -4,
+          created_at: 1477955052607
+        }
+      ];
+
+      const testRef = makeRefObj(testArticles);
+      const actual = formatComments(testComments, testRef);
+      expect(actual[0]).to.not.have.keys("belongs_to");
+      expect(actual[0]).to.contain.keys("article_id");
+    });
+
+    it("The value of the new article_id key must be the id corresponding to the original title value provided", () => {
+      const testArticles = [
+        {
+          article_id: 1,
+          title: "Agility Training....",
+          topic: "cooking",
+          author: "cooljmessy",
+          body: "The first day I pu...",
+          created_at: 1481662720516
+        },
+        {
+          article_id: 2,
+          title: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          topic: "cooking",
+          author: "tickle122",
+          body: "The chef Richst...",
+          created_at: 1492163783248
+        }
+      ];
+
+      const testComments = [
+        {
+          body: "Praesentium pariatur a nisi aut.",
+          belongs_to: "Agility Training....",
+          created_by: "grumpy19",
+          votes: 11,
+          created_at: 1462414995595
+        },
+        {
+          body: "Voluptatum aut fac",
+          belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          created_by: "weegembump",
+          votes: -4,
+          created_at: 1477955052607
+        }
+      ];
+
+      const testRef = makeRefObj(testArticles);
+      const actual = formatComments(testComments, testRef);
+
+      expect(actual[0].article_id).to.equal(1);
+      expect(actual[1].article_id).to.equal(2);
+    });
+
+    it("Its created_at value converted into a javascript date object", () => {
+      const testArticles = [
+        {
+          article_id: 1,
+          title: "Agility Training....",
+          topic: "cooking",
+          author: "cooljmessy",
+          body: "The first day I pu...",
+          created_at: 1481662720516
+        },
+        {
+          article_id: 2,
+          title: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          topic: "cooking",
+          author: "tickle122",
+          body: "The chef Richst...",
+          created_at: 1492163783248
+        }
+      ];
+
+      const testComments = [
+        {
+          body: "Praesentium pariatur a nisi aut.",
+          belongs_to: "Agility Training....",
+          created_by: "grumpy19",
+          votes: 11,
+          created_at: 1462414995595
+        },
+        {
+          body: "Voluptatum aut fac",
+          belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          created_by: "weegembump",
+          votes: -4,
+          created_at: 1477955052607
+        }
+      ];
+
+      const testRef = makeRefObj(testArticles);
+      const actual = formatComments(testComments, testRef);
+      expect(actual[0].created_at).to.be.an.instanceOf(Date);
+    });
+
+    it("Its maintains the rest of the comment properties", () => {
+      const testArticles = [
+        {
+          article_id: 1,
+          title: "Agility Training....",
+          topic: "cooking",
+          author: "cooljmessy",
+          body: "The first day I pu...",
+          created_at: 1481662720516
+        },
+        {
+          article_id: 2,
+          title: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          topic: "cooking",
+          author: "tickle122",
+          body: "The chef Richst...",
+          created_at: 1492163783248
+        }
+      ];
+
+      const testComments = [
+        {
+          body: "Praesentium pariatur a nisi aut.",
+          belongs_to: "Agility Training....",
+          created_by: "grumpy19",
+          votes: 11,
+          created_at: 1462414995595
+        },
+        {
+          body: "Voluptatum aut fac",
+          belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+          created_by: "weegembump",
+          votes: -4,
+          created_at: 1477955052607
+        }
+      ];
+
+      const testRef = makeRefObj(testArticles);
+      const actual = formatComments(testComments, testRef);
+      expect(actual[0]).to.have.all.keys(
+        "body",
+        "article_id",
+        "votes",
+        "created_at",
+        "author"
+      );
+    });
+  });
+});
