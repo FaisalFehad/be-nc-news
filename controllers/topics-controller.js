fdb2829488140250d596928235cb11ad3955fca9
FaisalFehad <faisal.fehad@pm.me>
2019-12-15 11:07:05 +0000

    handle error 402 no content

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,7 +1,9 @@
 const { fetchAllTopics } = require("../models/topics-model");
 
 exports.getTopics = (req, res, next) => {
-  fetchAllTopics().then(topics => {
-    res.status(200).send({ topics });
-  });
+  fetchAllTopics()
+    .then(topics => {
+      res.status(200).send({ topics });
+    })
+    .catch(next);
 };
