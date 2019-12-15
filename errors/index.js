c5752d67bdbc17017df4d9960344003379a57a61
FaisalFehad <faisal.fehad@pm.me>
2019-12-15 11:06:15 +0000

    handle 402

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,7 @@
 exports.handle404 = (req, res, next) => {
   res.status(404).send({ msg: "Not found" });
 };
+
+exports.handle402 = (req, res, next) => {
+  res.status(402).send({ msg: "No Content" });
+};
--- models/topics-model.js
+++ models/topics-model.js
@@ -1,5 +1,12 @@
 const connection = require("../db/connection");
+const { handle402 } = require("../errors/index");
 
 exports.fetchAllTopics = () => {
-  return connection.select("*").from("topics");
+  return connection
+    .select("*")
+    .from("topics")
+    .then(topics => {
+      if (!topics) handle402();
+      else return topics;
+    });
 };
