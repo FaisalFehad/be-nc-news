45663329534df74af9ddbcda0048c64bd7418b4b
FaisalFehad <faisal.fehad@pm.me>
2019-12-17 20:49:35 +0000

    add missing catch for error handling

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -7,11 +7,13 @@ const { fetchCommentsByArticleId } = require("../models/comments-model");
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
-  fetchArticleById(article_id).then(articleArr => {
-    const article = articleArr[0];
-    if (article) res.status(200).send({ article });
-    else res.status(404).send({ msg: "The article is not found" });
-  });
+  fetchArticleById(article_id)
+    .then(articleArr => {
+      const article = articleArr[0];
+      if (article) res.status(200).send({ article });
+      else res.status(404).send({ msg: "The article is not found" });
+    })
+    .catch(next);
 };
 
 exports.changeArticleVotes = (req, res, next) => {
