308b71a0d85dcc10c360c288ca9a22664eeccb33
FaisalFehad <faisal.fehad@pm.me>
2019-12-19 09:52:21 +0000

    refactor getArticleById to make use of promise chain rejection and next

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -11,10 +11,8 @@ exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
   fetchArticleById(article_id)
-    .then(articleArr => {
-      const article = articleArr[0];
-      if (article) res.status(200).send({ article });
-      else res.status(404).send({ msg: "The article is not found" });
+    .then(article => {
+      res.status(200).send({ article });
     })
     .catch(next);
 };
--- models/articles_model.js
+++ models/articles_model.js
@@ -8,7 +8,15 @@ exports.fetchArticleById = article_id => {
     .from("articles")
     .where({ "articles.article_id": article_id })
     .leftJoin("comments", "comments.article_id", "=", "articles.article_id")
-    .groupBy("articles.article_id");
+    .groupBy("articles.article_id")
+    .then(([article]) => {
+      if (article) return article;
+      else
+        return Promise.reject({
+          status: 404,
+          msg: "The article is not found"
+        });
+    });
 };
 
 exports.updateArticleVotes = (article_id, votesUpdate) => {
