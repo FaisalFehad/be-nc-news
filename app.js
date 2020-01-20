a67b01950c3e55283df6f26fa00e8c86b9cea668
FaisalFehad <faisal.fehad@pm.me>
2020-01-20 10:36:29 +0000

    add cors package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/api-router");
 const {
@@ -9,6 +10,7 @@ const {
   handle500
 } = require("./errors/index");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
